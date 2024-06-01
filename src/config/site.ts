91397abc179b9de7bf914568f2ff4d68217d5e00
danielrdsdev <danielrds.dev@gmail.com>
2024-06-01 20:02:42 -0300

    fix: siteName property

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
 	? `${process.env.NEXT_PUBLIC_VERCEL_URL}`
 	: 'http://localhost:3000'
 
-const baseName = 'TMDB Movies'
+const baseName = process.env.SITE_NAME
 
 export const siteConfig = {
 	metadata: {
