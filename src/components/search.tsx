d58bfa04b52a80338ddbe3e03bf3d9f46348da86
danielrdsdev <danielrds.dev@gmail.com>
2024-12-21 17:31:18 -0300

    fix: search component

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,19 +1,14 @@
-'use client'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import Form from 'next/form'
-import { useSearchParams } from 'next/navigation'
 
 export const Search = () => {
-	const searchParams = useSearchParams()
-
 	return (
 		<Form action="/search" className="relative flex items-center pb-4 w-full">
 			<Input
 				name="query"
 				placeholder="Buscar por um filme"
 				className="flex-1 w-full"
-				defaultValue={searchParams.get('query')?.toString()}
 			/>
 			<Button
 				type="submit"
